66382881dc52204fa96e481c3e0adf67287a8e54
Carlos Ignacio Roa <60223426+CIgnacio-dev@users.noreply.github.com>
2020-11-11 00:07:26 -0300

    Selected
    Ahora se puede seleccionar color del producto

--- index.js
+++ index.js
@@ -3,21 +3,29 @@ const app = new Vue({
     data:{
         product:{
             name: 'Headphones Sony WH-1000XM3',
-            quantity: '10',
-            colors:[
-             {
+            selected:{
+                id: 1,
                 name: 'black',
-                code: '#111111',
+                color: '#111111',
                 img: 'https://clsonyb2c.vteximg.com.br/arquivos/ids/344224-550-550/WH-1000XM3-0.jpg?v=637157205679400000',
+                cantidad: 100,
+            },
+            models:[
+             {
+                id: 1,
+                name: 'black',
+                color: '#111111',
+                img: 'https://clsonyb2c.vteximg.com.br/arquivos/ids/344224-550-550/WH-1000XM3-0.jpg?v=637157205679400000',
+                cantidad: 100,
             },
             {
+                id: 2,
                 name: 'silver',
-                code: '#0002',
+                color: '#0002',
                 img: 'https://static.bhphoto.com/images/images2000x2000/1541087780_1432360.jpg',
-
+                cantidad: 100,
             },
         ]
         },
-    }
-
+}
 })
