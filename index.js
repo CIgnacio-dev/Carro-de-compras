87e269a3306e3f7c6150da34ea30562d962e0e88
Carlos Ignacio Roa <60223426+CIgnacio-dev@users.noreply.github.com>
2020-11-12 06:38:54 -0300

    Carrito de compra
    Agregada la opción de agregar producto al carro de compras

--- index.js
+++ index.js
@@ -1,31 +1,82 @@
 const app = new Vue({
     el: '#app',
-    data:{
-        product:{
+    data: {
+        selectedProductId: undefined,
+        previewProductId: undefined,
+        selectedImageIndex: 0,
+        cart: [],
+        product: {
             name: 'Headphones Sony WH-1000XM3',
-            selected:{
-                id: 1,
-                name: 'black',
-                color: '#111111',
-                img: 'https://clsonyb2c.vteximg.com.br/arquivos/ids/344224-550-550/WH-1000XM3-0.jpg?v=637157205679400000',
-                cantidad: 100,
-            },
-            models:[
-             {
-                id: 1,
-                name: 'black',
-                color: '#111111',
-                img: 'https://clsonyb2c.vteximg.com.br/arquivos/ids/344224-550-550/WH-1000XM3-0.jpg?v=637157205679400000',
-                cantidad: 100,
-            },
-            {
-                id: 2,
-                name: 'silver',
-                color: '#0002',
-                img: 'https://static.bhphoto.com/images/images2000x2000/1541087780_1432360.jpg',
-                cantidad: 100,
-            },
-        ]
+            models: [
+                {
+                    id: 1,
+                    name: 'Black',
+                    color: '#111111',
+                    images: ['https://clsonyb2c.vteximg.com.br/arquivos/ids/344224-550-550/WH-1000XM3-0.jpg?v=637157205679400000',
+                    'https://reviewed-com-res.cloudinary.com/image/fetch/s--zWbescGk--/b_white,c_limit,cs_srgb,f_auto,fl_progressive.strip_profile,g_center,h_668,q_auto,w_1187/https://reviewed-production.s3.amazonaws.com/1566482682362/Sony-WH-1000XM3-Hero.jpg',
+                    'https://technologytoday.club/wp-content/uploads/2020/08/Sony-WH-1000XM4-vs-Sony-WH-1000XM3-%C2%BFvale-la-pena-actualizarlo.jpg',
+                    'https://www.gizlogic.com/wp-content/uploads/2019/06/Sony-WH-1000XM3.jpg'
+                ],
+                    cantidad: 100,
+                    descripcion: ['Los #audífonos #WH1000XM3 de #Sony te permiten sumergirte aún más en el silencio con las nuevas mejoras en la tecnología noise cancelling líder del sector y el audio inteligente que se ajusta a la situación.',
+                        'La exclusiva optimización de la presión atmosférica permite disfrutar de un sonido óptimo a grandes alturas, para que puedas disfrutar del noise cancelling a la perfección cuando viajes en avión.',
+                    ],
+                },
+                {
+                    id: 2,
+                    name: 'Silver',
+                    color: '#0002',
+                    images: ['https://static.bhphoto.com/images/images2000x2000/1541087780_1432360.jpg',
+                    'https://technologytoday.club/wp-content/uploads/2020/08/Sony-WH-1000XM4-vs-Sony-WH-1000XM3-%C2%BFvale-la-pena-actualizarlo.jpg',
+                    'https://www.gizlogic.com/wp-content/uploads/2019/06/Sony-WH-1000XM3.jpg'
+                ],
+                    cantidad: 0,
+                    descripcion: ['Los #audífonos #WH1000XM3 de #Sony te permiten sumergirte aún más en el silencio con las nuevas mejoras en la tecnología noise cancelling líder del sector y el audio inteligente que se ajusta a la situación.',
+                        'La exclusiva optimización de la presión atmosférica permite disfrutar de un sonido óptimo a grandes alturas, para que puedas disfrutar del noise cancelling a la perfección cuando viajes en avión.',
+                    ],
+                },
+            ]
         },
-}
+
+    },
+
+    computed: {
+    selectedProduct() {
+        if (this.selectedProductId){
+            return this.product.models.find(p => p.id === this.selectedProductId)
+        }
+        return this.product.models[0]
+    },
+    selectedProductImg(){
+        if (this.previewProductId){
+            return this.product.models.find(p => p.id === this.previewProductId).images[0]
+        }
+        return this.selectedProduct.images[this.selectedImageIndex]
+    },
+    cartAmount(){
+        return this.cart.length
+    }
+    
+},
+    methods: {
+        selectProduct(modelId){
+            this.selectedProductId = modelId
+            this.selectedImageIndex = 0
+    },
+        setPreviewModel(modelId){
+            this.previewProductId = modelId
+        },
+        clearPreviewModel(){
+            this.previewProductId = undefined
+        },
+        selectImage(index){
+            this.selectedImageIndex = index
+        },
+        addToCart(){
+            if (this.selectedProduct.cantidad >0){
+                this.cart.push(this.selectedProduct)
+            }
+        },
+        
+    }
 })
